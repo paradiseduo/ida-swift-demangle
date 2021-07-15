e10c470a8f4d3b27c2d997b9bcd1e6aaa7020863
scriptalert('1')/script <14846965+paradiseduo@users.noreply.github.com>
2021-07-15 16:45:40 +0800

    Create swift.py

---
+++ swift.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import idaapi
+import idc
+import idautils
+import subprocess
+
+
+class Swift_Dump_t(idaapi.plugin_t):
+    comment = "Swift Dump plugin for IDA Pro 7.0"
+    help = ""
+    wanted_name = "Swift Dump"
+    wanted_hotkey = "Ctrl-Alt-D"
+    flags = idaapi.PLUGIN_KEEP
+
+    def init(self):
+        return idaapi.PLUGIN_OK
+
+    def term(self):
+        return idaapi.PLUGIN_OK
+
+    def patcher(self):
+        AllFunc = []
+        AllFuncName = []
+        for func in idautils.Functions():
+            name = GetFunctionName(func)
+            AllFunc += [func]
+            if name[:1] == "_":
+                name = name[1:]
+            AllFuncName += [name]
+
+        proc = subprocess.Popen('xcrun swift-demangle', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        proc.stdin.write('\n'.join(AllFuncName))
+        proc.stdin.close()
+        results = proc.stdout.read().split('\n')
+        proc.wait()
+        i = 0
+        for addr in AllFunc:
+            old_name = AllFuncName[i]
+            new_name = results[i]
+            if new_name != old_name:
+                MakeNameEx(addr, new_name, SN_NOCHECK|SN_NOWARN)
+                SetFunctionCmt(addr, "%s \n%s" % (old_name, new_name), 1)
+            i += 1
+
+    def run(self, arg):
+        self.patcher()
+
+
+# register IDA plugin
+def PLUGIN_ENTRY():
+    return Swift_Dump_t()
